bed06455abe80582e9ea9cdb394d4b8a3ddb4fd8
Firstname Lastname <jmbwebco@gmail.com>
2020-07-22 13:51:45 +0100

    updates

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const button = document.getElementById("enter");
 const input = document.getElementById("userinput");
 const ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-	if (input.value.length > 0) {
-	const li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-	}
-})
+function inputLength() {
+	return input.value.length;
+}
 
-input.addEventListener("keypress", function(event) {
-	if (input.value.length > 0 && event.keyCode === 13) {
+function createListElement() {
 	const li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
-	}
-})
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) 
+	createListElement();
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13)
+	createListElement();
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);

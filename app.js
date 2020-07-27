cfc29cb0fc27d388087f956d3b3131ea0a47def0
Firstname Lastname <jmbwebco@gmail.com>
2020-07-27 10:46:36 +0100

    adding delete function

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ function createListElement() {
 	const li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
+	//delete button
+	const trashButton = document.createElement('button');
+	trashButton.innerHTML = `&#10006`;
+	trashButton.classList.add("trash-btn");
+	//append to list
+	li.appendChild(trashButton);
+	//clear todo input value
 	input.value = "";
 }
 
@@ -23,6 +30,17 @@ function addListAfterKeypress(event) {
 	createListElement();
 }
 
+function deleteItem(e) {
+	const item = e.target;
+	//delete todo
+	if (item.classList[0] === "trash-btn") {
+		const todo = item.parentElement;
+		todo.remove();
+	}
+}
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", deleteItem);
